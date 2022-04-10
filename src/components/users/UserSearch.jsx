c15a17ff0616ad1708dba6b81be85a23be47d6e5
Manuel Brestak <manuel.brestak@gmail.com>
2022-04-10 20:05:29 +0200

    57. Search Users

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -4,7 +4,7 @@ import GithubContext from '../../context/github/GithubContext';
 function UserSearch() {
   const [text, setText] = useState('');
 
-  const { users } = useContext(GithubContext);
+  const { users, searchUsers } = useContext(GithubContext);
 
   const handleChange = (e) => {
     setText(e.target.value);
@@ -16,8 +16,8 @@ function UserSearch() {
     if (text === '') {
       alert('Please enter something.')
     } else {
-      // @todo - search users
-
+      searchUsers(text);
+      
       setText('');
     }
   }
@@ -38,7 +38,7 @@ function UserSearch() {
               <button type="submit" className="w-24 btn btn-lg">
                 Go
               </button>
-              </div>
+            </div>
           </div>
         </form>
       </div>
