ae45d68e16e7dc0192b3f60f4d0e83f76d96a9fa
Manuel Brestak <manuel.brestak@gmail.com>
2022-04-20 13:33:13 +0200

    added netlify functions

---
+++ functions/getUserAndRepos.js
@@ -0,0 +1,26 @@
+const github = require('./utils/github');
+
+const handler = async event => {
+  const login = event.queryStringParameters.q;
+  try {
+    const [user, repos] = await Promise.all([
+      github.get(`/users/${login}`),
+      github.get(`/users/${login}/repos`),
+    ]);
+
+    return {
+      headers: { 'Content-Type': 'application/json' },
+      statusCode: 200,
+      body: JSON.stringify({ user: user.data, repos: repos.data }),
+    };
+  } catch (error) {
+    const { status, statusText, headers, data } = error.response;
+
+    return {
+      statusCode: status,
+      body: JSON.stringify({ status, statusText, headers, data }),
+    };
+  }
+};
+
+module.exports = { handler };
---
+++ functions/searchUsers.js
@@ -0,0 +1,23 @@
+const github = require('./utils/github');
+
+const handler = async event => {
+  const params = new URLSearchParams(event.queryStringParameters);
+  try {
+    const response = await github.get(`/search/users?${params}`);
+
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(response.data.items),
+    };
+  } catch (error) {
+    const { status, statusText, headers, data } = error.response;
+
+    return {
+      statusCode: status,
+      body: JSON.stringify({ status, statusText, headers, data }),
+    };
+  }
+};
+
+module.exports = { handler };
